1727158111052b3e82338a178902c48f782be8ec
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2017-10-23 19:52:13 +0300

    Move CfgNode definitions to cpp

--- node_parser/cfg_node.cpp
+++ node_parser/cfg_node.cpp
@@ -1,6 +1,118 @@
 #include "cfg_node.hpp"
 #include <iostream>
 
+CfgNode::CfgNode(const std::string& name) : name(name), properties(), children()
+{
+}
+
+CfgNode::CfgNode(const CfgNode& node)
+{
+    name = node.getName();
+    copyFrom(node);
+}
+
+CfgNode& CfgNode::operator=(const CfgNode& node)
+{
+    name = node.getName();
+    copyFrom(node);
+    return *this;
+}
+
+CfgNode::CfgNode(CfgNode&&) = default;
+CfgNode& CfgNode::operator=(CfgNode&&) = default;
+CfgNode::~CfgNode() = default;
+
+const std::string& CfgNode::getName() const { return name; }
+
+bool CfgNode::hasChild(const std::string& name) const
+{
+    return std::find_if(children.begin(), children.end(),
+                        [&name](const auto& child) {
+                            return name == child->getName();
+                        }) != children.end();
+}
+
+CfgNode& CfgNode::addChild(const std::string& name)
+{
+    if (hasChild(name))
+        throw std::logic_error("Dupplicate key");
+    children.push_back(std::make_unique<CfgNode>(name));
+    return *children.back();
+}
+
+void CfgNode::copyFrom(const CfgNode& node)
+{
+    properties.insert(node.begin(), node.end());
+    for (const auto& child : node.children)
+        children.push_back(std::make_unique<CfgNode>(*child.get()));
+}
+
+size_t CfgNode::getNumChildren() const { return children.size(); }
+
+const CfgNode& CfgNode::getChild(size_t n) const { return *children[n]; }
+
+CfgNode& CfgNode::getChild(size_t n)
+{
+    const auto* c = static_cast<const CfgNode*>(this);
+    return const_cast<CfgNode&>(c->getChild(n));
+}
+
+const CfgNode& CfgNode::getChild(const std::string& name) const
+{
+    auto it = find_child(name);
+    return *it->get();
+}
+
+CfgNode& CfgNode::getChild(const std::string& name)
+{
+    const auto* c = static_cast<const CfgNode*>(this);
+    return const_cast<CfgNode&>(c->getChild(name));
+}
+
+void CfgNode::setProperty(const std::string& name, const std::string& value)
+{
+    properties[name] = value;
+}
+
+void CfgNode::appendToPropperty(const std::string& name,
+                                const std::string& value)
+{
+    properties[name] += value;
+}
+
+const std::string& CfgNode::getProperty(const std::string& name) const
+{
+    return properties.at(name);
+}
+
+double CfgNode::getPropertyAsDouble(const std::string& name) const
+{
+    return std::stod(getProperty(name));
+}
+
+long long CfgNode::getPropertyAsLong(const std::string& name) const
+{
+    return std::stoll(getProperty(name));
+}
+
+CfgNode::property_it CfgNode::begin() { return properties.begin(); }
+CfgNode::property_it CfgNode::end() { return properties.end(); }
+CfgNode::property_cit CfgNode::begin() const { return properties.cbegin(); }
+CfgNode::property_cit CfgNode::end() const { return properties.cend(); }
+
+CfgNode::children_cit CfgNode::find_child(const std::string& name) const
+{
+    return std::find_if(
+        children.begin(), children.end(),
+        [&name](const auto& child) { return name == child->getName(); });
+}
+
+CfgNode::children_it CfgNode::find_child(const std::string& name)
+{
+    const auto cit = static_cast<const CfgNode*>(this)->find_child(name);
+    return children.erase(cit, cit);
+}
+
 namespace {
 void print_node(std::ostream& str, const CfgNode& node, int tab_width = 4,
                 int lvl = 0)
--- node_parser/cfg_node.hpp
+++ node_parser/cfg_node.hpp
@@ -14,116 +14,44 @@ public:
     using children_cit = children_container::const_iterator;
     using children_it = children_container::iterator;
 
-    CfgNode(const std::string& name) : name(name), properties(), children() {}
-    CfgNode(const CfgNode& node)
-    {
-        name = node.getName();
-        copyFrom(node);
-    }
-    CfgNode& operator=(const CfgNode& node)
-    {
-        name = node.getName();
-        copyFrom(node);
-        return *this;
-    }
-    CfgNode(CfgNode&&) = default;
-    CfgNode& operator=(CfgNode&&) = default;
-    ~CfgNode() = default;
+    CfgNode(const std::string& name);
+    CfgNode(const CfgNode& node);
+    CfgNode& operator=(const CfgNode& node);
+    CfgNode(CfgNode&&);
+    CfgNode& operator=(CfgNode&&);
+    ~CfgNode();
 
-    const std::string& getName() const { return name; }
+    const std::string& getName() const;
+    bool hasChild(const std::string& name) const;
+    CfgNode& addChild(const std::string& name);
+    void copyFrom(const CfgNode& node);
 
-    bool hasChild(const std::string& name) const
-    {
-        return std::find_if(children.begin(), children.end(),
-                            [&name](const auto& child) {
-                                return name == child->getName();
-                            }) != children.end();
-    }
+    size_t getNumChildren() const;
+    const CfgNode& getChild(size_t n) const;
+    CfgNode& getChild(size_t n);
 
-    CfgNode& addChild(const std::string& name)
-    {
-        if (hasChild(name))
-            throw std::logic_error("Dupplicate key");
-        children.push_back(std::make_unique<CfgNode>(name));
-        return *children.back();
-    }
+    const CfgNode& getChild(const std::string& name) const;
+    CfgNode& getChild(const std::string& name);
 
-    void copyFrom(const CfgNode& node)
-    {
-        properties.insert(node.begin(), node.end());
-        for (const auto& child : node.children)
-            children.push_back(std::make_unique<CfgNode>(*child.get()));
-    }
+    void setProperty(const std::string& name, const std::string& value);
+    void appendToPropperty(const std::string& name, const std::string& value);
 
-    size_t getNumChildren() const { return children.size(); }
+    const std::string& getProperty(const std::string& name) const;
+    double getPropertyAsDouble(const std::string& name) const;
+    long long getPropertyAsLong(const std::string& name) const;
 
-    const CfgNode& getChild(size_t n) const { return *children[n]; }
-
-    CfgNode& getChild(size_t n)
-    {
-        const auto* c = static_cast<const CfgNode*>(this);
-        return const_cast<CfgNode&>(c->getChild(n));
-    }
-
-    const CfgNode& getChild(const std::string& name) const
-    {
-        auto it = find_child(name);
-        return *it->get();
-    }
-
-    CfgNode& getChild(const std::string& name)
-    {
-        const auto* c = static_cast<const CfgNode*>(this);
-        return const_cast<CfgNode&>(c->getChild(name));
-    }
-
-    void setProperty(const std::string& name, const std::string& value)
-    {
-        properties[name] = value;
-    }
-
-    void appendToPropperty(const std::string& name, const std::string& value)
-    {
-        properties[name] += value;
-    }
-
-    const std::string& getProperty(const std::string& name) const
-    {
-        return properties.at(name);
-    }
-
-    double getPropertyAsDouble(const std::string& name) const
-    {
-        return std::stod(getProperty(name));
-    }
-
-    long long getPropertyAsLong(const std::string& name) const
-    {
-        return std::stoll(getProperty(name));
-    }
-
-    property_it begin() { return properties.begin(); }
-    property_it end() { return properties.end(); }
-    property_cit begin() const { return properties.cbegin(); }
-    property_cit end() const { return properties.cend(); }
+    property_it begin();
+    property_it end();
+    property_cit begin() const;
+    property_cit end() const;
 
 private:
     std::string name;
     properties_type properties;
     std::vector<std::unique_ptr<CfgNode>> children;
 
-    children_cit find_child(const std::string& name) const
-    {
-        return std::find_if(
-            children.begin(), children.end(),
-            [&name](const auto& child) { return name == child->getName(); });
-    }
-
-    children_it find_child(const std::string& name)
-    {
-        const auto cit = static_cast<const CfgNode*>(this)->find_child(name);
-        return children.erase(cit, cit);
-    }
+    children_cit find_child(const std::string& name) const;
+    children_it find_child(const std::string& name);
 };
 
 std::ostream& operator<<(std::ostream& str, const CfgNode& node);
